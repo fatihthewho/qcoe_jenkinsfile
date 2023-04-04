bdcfa0cc924f756d97fb165c8f94cc15f562e4af
venkatakunta <venkata.kunta@insightsoftware.com>
2023-04-04 14:45:32 +0530

    Update helpers.groovy

--- src/helpers.groovy
+++ src/helpers.groovy
@@ -14,7 +14,7 @@ def checkoutRepo(url,branch){
 		
 }
 def executeMavenTests(threadCount,isRemote,browser,environment,retries,xmlFileName) {
-    bat 'mvn test -DthreadCount=${threadCount} -Dremote=${isRemote} -DBrowser=${browser} -Denv=${environment} -Dretry=${retries} -DsuiteFile=${xmlFileName}'
+    bat 'mvn test -DthreadCount="${threadCount}" -Dremote=${isRemote} -DBrowser=${browser} -Denv=${environment} -Dretry=${retries} -DsuiteFile=${xmlFileName}'
 }
 def archiveArtifacts() {
     archiveArtifacts "${PROJECT_LOCATION}/bin/${SLN_CONFIG}/logs/**/*.*"
