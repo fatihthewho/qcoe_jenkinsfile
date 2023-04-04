52b9295c60a215bb262d9f8d908d51819605d2de
venkatakunta <venkata.kunta@insightsoftware.com>
2023-04-04 16:48:05 +0530

    Testing

--- src/helpers.groovy
+++ src/helpers.groovy
@@ -17,14 +17,36 @@ def executeMavenTests(threads,isRemote,browser,environment,retries,xmlFileName) 
   
     bat "mvn test -DthreadCount=${threads} -Dremote=${isRemote} -DBrowser=${browser} -Denv=${environment} -Dretry=${retries} -DsuiteFile=${xmlFileName}"
 }
-def archiveArtifacts() {
-    archiveArtifacts "${PROJECT_LOCATION}/bin/${SLN_CONFIG}/logs/**/*.*"
-    if(ARTIFACT_PATTERN.length()>0){
-        for(artifactPath in ARTIFACT_PATTERN.split(';')){
-            artifactFileName = artifactPath.tokenize('\\').last()
-            projectDirNoSlash = "${PROJECT_LOCATION}".substring(1,"${PROJECT_LOCATION}".length())
-            archiveArtifacts "${projectDirNoSlash}/bin/${SLN_CONFIG}/${artifactFileName}"
-        }
-    }
+def archiveJavaArtifacts() {
+	archiveArtifacts allowEmptyArchive: true, artifacts: '__test-results\\Report.html', followSymlinks: false
+	testNG showFailedBuilds: true
+	publishHTML([allowMissing: false,alwaysLinkToLastBuild: false,keepAll: false,reportDir: '__test-results',reportFiles: 'Report.html',reportName: 'Test Summary',reportTitles: ''])
+            
+}
+def updateXRayWithTestNG(project,testPlan) {
+	if ("${jira_project_key}" != 'NA' ){
+			step(
+			[$class: 'XrayImportBuilder', endpointName: '/testng/multipart', importFilePath: '**/testng-results.xml', importInParallel: 'false', importInfo: '''{
+			"fields": {
+				"project": {
+					"key": "${project}"
+				},
+				"summary": "Test Summary from Jenkins Build-${JOB_BASE_NAME}#${BUILD_NUMBER}", 
+				"issuetype": {
+				   "name": "Test Execution"
+				}  
+			},
+			"xrayFields": {
+					"testPlanKey": "${testPlan}"
+				}
+			}''', importToSameExecution: 'false', inputInfoSwitcher: 'fileContent', inputTestInfoSwitcher: 'filePath', serverInstance: 'CLOUD-4d5d4a26-3cb7-4838-a9ff-1b25e9f1cf55']
+			)
+	}
+}
+def sendEmail(emailRecipients) {
+   if ("${emailRecipients}" != 'NA' ){
+	  emailext body: '${FILE,path="__test-results/email-report.html"}', mimeType: 'text/html', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS', to: '${email_recipients}'
+   }
+             
 }
 return this
